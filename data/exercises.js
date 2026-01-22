727c8769e3321a72c9f7d4322d7e41178b68b81a
Priyam <priyamdevsingh9@gmail.com>
2026-01-23 00:20:42 +0530

    Added exercises

---
+++ data/exercises.js
@@ -0,0 +1,88 @@
+export const exercises = [
+    {
+        'id' : 'ex_01',
+        'name' : 'Chest Flyes',
+        'muscleGroup' : 'Chest',
+        'description': 'Chest Flyes either with Dumbbell or on a machine',
+    },
+    {
+        'id': 'ex_02',
+        'name': 'Incline Press',
+        'muscleGroup' : 'Chest',
+        'description': 'Incline Chest Press either free weights or a machine',
+    },
+    {
+        'id':'ex_03',
+        'name':'Straight Arm PullDowns',
+        'muscleGroup':'Back',
+        'description':'Straight arm Lat Pulldowns on a Lat pulldown machine',
+    },
+    {
+        'id':'ex_04',
+        'name':'Palms-up Pulldown',
+        'muscleGroup':'Back',
+        'description': 'Close grip palms up pulldown on a Lat pulldown machine',
+    },
+    {
+        'id':'ex_05',
+        'name':'Deadlift',
+        'muscleGroup':'Back',
+        'description': 'Conventional Deadlift with shoulder width stance',
+    },
+    {
+        'id':'ex_06',
+        'name':'Leg Extensions',
+        'muscleGroup':'Legs',
+        'description': 'Leg Extensions on a machine',
+    },
+    {
+        'id':'ex_07',
+        'name':'Leg Press',
+        'muscleGroup':'Legs',
+        'description': 'Leg Press either seated or laying down',
+    },
+    {
+        'id':'ex_08',
+        'name':'Calf Raises',
+        'muscleGroup':'Legs',
+        'description': 'Standing Calf raises preferably on a machine',
+    },
+    {
+        'id':'ex_09',
+        'name':'Sit ups',
+        'muscleGroup':'Abs',
+        'description': 'Sit ups preferably on a supported machine',
+    },
+    {
+        'id':'ex_10',
+        'name':'Lateral Raises',
+        'muscleGroup':'Shoulder',
+        'description': 'Shoulder Lateral Raises either free weight or on a machine',
+    },
+    {
+        'id':'ex_11',
+        'name':'Rear Delt Fly',
+        'muscleGroup':'Shoulder',
+        'description': 'Rear Delt flyes preferably on a machine',
+    },
+    {
+        'id':'ex_12',
+        'name':'Close Grip Pulldown',
+        'muscleGroup':'Biceps',
+        'description': 'Close grip palms up pulldown on a Lat pulldown machine',
+    },
+    {
+        'id':'ex_13',
+        'name':'Tricep Pressdown',
+        'muscleGroup':'Triceps',
+        'description': 'Tricep press downs on a machine',
+    },
+    
+    {
+        'id':'ex_14',
+        'name':'Dips',
+        'muscleGroup':'Chest',
+        'description': 'Standard bar dips or bench dips',
+    },
+    
+]
